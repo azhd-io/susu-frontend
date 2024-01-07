9c5807be6118e4d8bdd77c4923d0d1e8954fefb8
azhd-io <aliffazhad.io@gmail.com>
2024-01-07 20:46:45 +0800

    deploy2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-  extends: ["next/core-web-vitals"]
+  extends: ["next/core-web-vitals"],
+  rules: {
+    // Disables the no-unescaped-entities rule
+    "react/no-unescaped-entities": "off",
+
+    // Disables the Next.js specific rule about using <img> instead of <Image />
+    "@next/next/no-img-element": "off",
+
+    // Add other rules you want to disable here
+  },
+  
 };
+
